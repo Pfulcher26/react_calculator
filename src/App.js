1d64a967d7771d1f9322f7dbe88e009537d31f13
pfulcher26 <pfulcher26@gmail.com>
2022-10-04 16:24:01 -0700

    Creates divs for calculator display components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">
+          <div className="current-operand">
+      <h1>hiya</h1>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
